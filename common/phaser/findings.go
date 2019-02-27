325aeb7c828cda7af9f5f2baf917d6dabbcfa8c6
z0mbie42 <sylvain@kerkour.com>
2019-02-27 17:35:44 +0100

    ?

--- common/phaser/findings.go
+++ common/phaser/findings.go
@@ -1,14 +1,12 @@
 package phaser
 
-// type Findings struct {
-// 	Ports []findings.Port `json:"ports"`
-// 	CNAME *string         `json:"cname"`
-// }
-
+// Finding wraps a module result. Data is the result of a module and MUST be a type found in
+// phaser/findings
 type Finding struct {
 	Module  string      `json:"module"`
 	Version string      `json:"version"`
 	Data    interface{} `json:"data"`
 }
 
+// Findings is a collection of Finding
 type Findings []Finding
--- common/phaser/messages.go
+++ common/phaser/messages.go
@@ -4,11 +4,15 @@ import (
 	"time"
 )
 
+// ScanCompletedMessage notifies kernel that a scan has completed and results are available to be
+// retrieved
+// phaser_worker -> kernel
 type ScanCompletedMessage struct {
 	ReportID string `json:"report_id"`
 	File     File   `json:"file"`
 }
 
+// File is a file, with hashs to ensure integrity
 type File struct {
 	Path   string `json:"path"`
 	MD5    string `json:"md5,omitempty"`
@@ -17,6 +21,8 @@ type File struct {
 	SHA512 string `json:"sha512,omitempty"`
 }
 
+// ScanQueuedMessage notifies phaser workers that a scan has been requested
+// kernel -> phaser_worker
 type ScanQueuedMessage struct {
 	ScanID   string   `json:"scan_id"`
 	Targets  []string `json:"targets"`
@@ -24,6 +30,8 @@ type ScanQueuedMessage struct {
 	ReportID string   `json:"report_id"`
 }
 
+// ScanStartedMessage notifies kernel that a specific scan has started
+// phaser_worker -> kernel
 type ScanStartedMessage struct {
 	ReportID  string    `json:"report_id"`
 	StartedAt time.Time `json:"started_at"`
