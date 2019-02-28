2747d64a6e40b887b6aeaa54895cac1ffa08b0dc
z0mbie42 <sylvain@kerkour.com>
2019-02-28 22:11:24 +0100

    cli: Add clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use clap::{App};
+
 fn main() {
-    println!("Hello, world 2!");
+    App::new(clap::crate_name!())
+        .author(clap::crate_authors!())
+        .version(clap::crate_version!())
+        .about(clap::crate_description!())
+        .get_matches();
 }
