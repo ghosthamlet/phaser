eb211e642a7acba57d3112f9f6974f9e50fbe3ed
z0mbie42 <sylvain@kerkour.com>
2019-03-04 17:23:57 +0100

    worker: improve message enum

--- src/worker/messages.rs
+++ src/worker/messages.rs
@@ -2,45 +2,20 @@ use serde::{Serialize, Deserialize};
 
 // Message is used to send and receive messages between services
 #[derive(Clone, Debug, Deserialize, Serialize)]
-pub struct Message {
-    #[serde(rename = "type")]
-    pub type_: String,
-    pub data: MessageData,
+#[serde(tag = "type", content = "data")]
+pub enum Message {
+    // kernel -> phaser
+    ScanQueued{ scan_id: String, targets: Vec<String>, profile: String, report_id: String },
+    // phaser -> kernel
+    ScanStarted{ report_id: String, started_at: String },
+    ScanCompleted{ report_id: String, file: File },
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
-pub enum MessageData{
+pub struct File {
+    pub path: String,
+    pub md5: Option<String>,
+    pub sha1: Option<String>,
+    pub sha256: Option<String>,
+    pub sha512: Option<String>,
 }
-
-// // ScanCompletedMessage notifies kernel that a scan has completed and results are available to be
-// // retrieved
-// // phaser_worker -> kernel
-// type ScanCompletedMessage struct {
-// 	ReportID string `json:"report_id"`
-// 	File     File   `json:"file"`
-// }
-
-// // File is a file, with hashs to ensure integrity
-// type File struct {
-// 	Path   string `json:"path"`
-// 	MD5    string `json:"md5,omitempty"`
-// 	SHA1   string `json:"sha1,omitempty"`
-// 	SHA256 string `json:"sha256,omitempty"`
-// 	SHA512 string `json:"sha512,omitempty"`
-// }
-
-// // ScanQueuedMessage notifies phaser workers that a scan has been requested
-// // kernel -> phaser_worker
-// type ScanQueuedMessage struct {
-// 	ScanID   string   `json:"scan_id"`
-// 	Targets  []string `json:"targets"`
-// 	Profile  string   `json:"profile"`
-// 	ReportID string   `json:"report_id"`
-// }
-
-// // ScanStartedMessage notifies kernel that a specific scan has started
-// // phaser_worker -> kernel
-// type ScanStartedMessage struct {
-// 	ReportID  string    `json:"report_id"`
-// 	StartedAt time.Time `json:"started_at"`
-// }
