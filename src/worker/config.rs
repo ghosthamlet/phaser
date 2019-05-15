125cb8abeba806fa492b0fe31b181290e2432459
z0mbie42 <sylvain@kerkour.com>
2019-05-15 19:19:33 +0200

    worker: fix ocnfig by removing aws env vars

--- src/worker/config.rs
+++ src/worker/config.rs
@@ -5,10 +5,6 @@ use std::env;
 #[derive(Clone, Debug, Default, Serialize, Deserialize)]
 pub struct Config {
     pub environment: String,
-    pub aws_access_key_id: String,
-    pub aws_secret_access_key: String,
-    pub aws_region: String,
-    pub aws_s3_bucket: String,
     pub assets_folder: String,
     pub sentry_url: String,
     pub api_url: String,
@@ -26,11 +22,7 @@ fn get_env(var: &str) -> String {
 impl Config {
     pub fn new() -> Config {
         return Config{
-            environment: get_env("ENVIRONMENT"),
-            aws_access_key_id: get_env("AWS_ACCESS_KEY_ID"),
-            aws_secret_access_key: get_env("AWS_SECRET_ACCESS_KEY"),
-            aws_region: get_env("AWS_REGION"),
-            aws_s3_bucket: get_env("AWS_S3_BUCKET"),
+            environment: get_env("RUST_ENV"),
             assets_folder: get_env("ASSETS_FOLDER"),
             sentry_url: get_env("SENTRY_URL"),
             api_url: get_env("API_URL"),
