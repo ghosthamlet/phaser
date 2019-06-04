7a653c3053901f110cd8e265150631df178840f4
z0mbie42 <sylvain@kerkour.fr>
2019-06-04 09:18:28 +0200

    worker: Update concurrency

--- src/cli/worker.rs
+++ src/cli/worker.rs
@@ -11,6 +11,20 @@ pub fn run(_: &ArgMatches) -> Result<(), String> {
     let _sentry = sentry::init(env::var("SENTRY_URL").unwrap());
     sentry::integrations::panic::register_panic_handler();
 
-    Worker::new().run();
+
+    let (tx, rx) = mpsc::channel();
+    let n = 21;
+    for i in 0..n {
+        let tx1 = mpsc::Sender::clone(&tx);
+        thread::spawn(move || {
+            Worker::new(i).run();
+            tx1.send(i).unwrap();
+        });
+        thread::sleep(time::Duration::from_secs(1))
+    }
+
+    for received in rx {
+        println!("worker: {} terminated", received);
+    }
     Ok(())
 }
